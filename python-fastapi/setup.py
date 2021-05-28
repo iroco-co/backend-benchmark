d995c0ecad95a346f78dc582e32bf916637a18cb
Bruno Thomas <bruno@barreverte.fr>
2021-05-28 19:46:29 +0200

    first commit

---
+++ python-fastapi/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='classe',
+      version='0.1',
+      description="serveur de contact starlet/fastapi",
+      use_pipfile=True,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Starlet",
+          "Framework :: FastAPI",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      url='http://github.com/',
+      setup_requires=['setuptools-pipfile'],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite="nose.collector",
+      )
+
